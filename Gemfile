dadb59eb925d9c2b629aa6c40535457fdacbb056
Andrew Garver <a.w.garver@gmail.com>
2016-09-02 21:11:04 -0400

    basic devise set up, also letter_opener for testing emails

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  # For testing emails
+  gem 'letter_opener'
 end
 
 group :development do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
 
   # Default URL
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded on
