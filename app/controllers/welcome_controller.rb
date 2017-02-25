82bc4d6f0ff201ec5b16c00476f590f5a48b4e8a
Stefan Hartmann <stefanhartmann@gmail.com>
2017-02-25 14:35:28 -0500

    add scheduling mail you dope

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,11 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:account] == 'basic'
-      WelcomeMailer.scheduling_email(params).deliver
-    else
+    if params[:account] == 'trial'
       WelcomeMailer.welcome_email(params).deliver
       WelcomeMailer.client_email(params).deliver
+    else
+      WelcomeMailer.scheduling_email(params).deliver
     end
     render json: {status: 'Signup Successfully Sent'}
   end
