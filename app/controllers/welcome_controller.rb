ea1ea17d427513a0a091d5e94a26eb22539d97e5
Stefan Hartmann <stefanhartmann@gmail.com>
2017-02-25 16:04:28 -0500

    remove byebug from welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    byebug
     if params["account"] == 'trial'
       WelcomeMailer.welcome_email(params).deliver
       WelcomeMailer.client_email(params).deliver
