de43eddfff7496e0be113d42296d95ead224b395
Stefan Hartmann <stefanhartmann@gmail.com>
2017-03-02 23:01:54 -0500

    update flag_mailer

--- app/mailers/flag_mailer.rb
+++ app/mailers/flag_mailer.rb
@@ -5,6 +5,7 @@ class FlagMailer < ApplicationMailer
     @flag = flag
     @writer = Writer.find(@flag["writer_id"])
     @reason = @flag["flag_value"]
+    @comment = @flag["flag_comment"]
     assigned = User.find(@writer.user_id)
     @assigned = assigned.email
     mail(to: 'stefanhartmann@gmail.com', subject: 'FLAG: '+@writer.f_name+' '+@writer.l_name+' - '+@reason)
