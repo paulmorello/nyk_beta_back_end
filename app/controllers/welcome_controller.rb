1021f657922638e6c2b9a5e03212bb2b258cf3d8
Stefan Hartmann <stefanhartmann@gmail.com>
2017-03-25 17:39:46 -0400

    update mailer subject

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,11 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params["account"] == 'trial'
+    if params["account"] == 'Trial'
       # WelcomeMailer.welcome_email(params).deliver
       # WelcomeMailer.client_email(params).deliver
       WelcomeMailer.scheduling_email(params).deliver
-    elsif params["account"] == 'basic'
+    elsif params["account"] == 'Basic'
       WelcomeMailer.scheduling_email(params).deliver
     elsif params["account"] == 'mailingList'
       WelcomeMailer.mailing_list(params).deliver
